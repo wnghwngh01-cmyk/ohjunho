f7ddf00c1640559968b03a0291900e11bfc2812c
ohjunho <wnghwngh01@gmail.com>
2026-09-11 11:14:26 +0900

    Refresh deployed app cache immediately

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='personal-lab-shell-v10';
+const CACHE='personal-lab-shell-v11';
 const SHELL=['./','./index.html','./public.html','./privacy.html','./terms.html','./delete-account.html','./assets/styles.css','./js/config.js','./js/drafts.js','./js/db.js','./js/storage.js','./js/app.js','./js/project-files-ui.js','./js/public.js','./js/delete-account.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL.filter(Boolean))).catch(()=>{})));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL.filter(Boolean))).then(()=>self.skipWaiting()).catch(()=>{})));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   const req=event.request;
   if(req.method!=='GET')return;
