dda41cc12bf6b4739ff064d8ea835c26ec0e50b6
ohjunho <wnghwngh01@gmail.com>
2026-09-11 11:24:12 +0900

    Fix dark theme contrast

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='personal-lab-shell-v11';
+const CACHE='personal-lab-shell-v12';
 const SHELL=['./','./index.html','./public.html','./privacy.html','./terms.html','./delete-account.html','./assets/styles.css','./js/config.js','./js/drafts.js','./js/db.js','./js/storage.js','./js/app.js','./js/project-files-ui.js','./js/public.js','./js/delete-account.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL.filter(Boolean))).then(()=>self.skipWaiting()).catch(()=>{})));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
